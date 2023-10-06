9fb6f05fe83cab687be951ea2ca54dc3a003b732
Leo <volwahsenl@gmail.com>
2023-10-06 16:39:52 +0200

    Cards are now responsive in the way they order themselfs when width of screen is adjusted

--- frontend/my-supermarket/src/components/Card/index.jsx
+++ frontend/my-supermarket/src/components/Card/index.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import "./style.css";
 import { formatCurrency } from "../../utilities/formatCurrency";
 
@@ -13,12 +15,11 @@ function CardComponent({ products }) {
       setIsLoading(false);
     }, 2000);
 
-  
     return () => clearTimeout(timer);
   }, []);
 
   if (isLoading) {
-    return <h1>Loading...</h1>; 
+    return <h1>Loading...</h1>;
   }
 
   if (!products || products.length === 0) {
@@ -26,18 +27,23 @@ function CardComponent({ products }) {
   }
 
   return (
-    <div>
+    <Row>
       {products.map((product) => (
-        <Card style={{ width: "18rem" }} key={product._id}>
-          <Card.Img variant="top" src={product.image} />
-          <Card.Body>
-            <Card.Title>{product.name}</Card.Title>
-            <Card.Text>The cost is {formatCurrency(product.price)}</Card.Text>
-            <Button variant="primary">Add to shopping card</Button>
-          </Card.Body>
-        </Card>
+        <Col key={product._id} xs={12} sm={6} md={4} lg={3} className="mt-4">
+          <Card
+            style={{ width: "100%" }}
+            className="mt-auto"
+          >
+            <Card.Img variant="top" src={product.image} />
+            <Card.Body>
+              <Card.Title>{product.name}</Card.Title>
+              <Card.Text>The cost is {formatCurrency(product.price)}</Card.Text>
+              <Button variant="primary">Add to shopping card</Button>
+            </Card.Body>
+          </Card>
+        </Col>
       ))}
-    </div>
+    </Row>
   );
 }
 
